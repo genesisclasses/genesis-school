403865a7cf774e0657153ac16e091a6a566e055c
riyasham2151 <riyasham2151@gmail.com>
2025-10-30 06:28:11 +0000

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './global/Navbar'
-import Home from './components/Home'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<Navbar/>
-<Footer />
+
     </>
   )
 }
