309ed66fe5ac799303b06cc48d65e75870bd484d
riyasham2151 <riyasham2151@gmail.com>
2025-10-30 06:17:31 +0000

    structure created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './global/Navbar'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   <h1 className='text-red-400 text-xl'>hello</h1>
+<Navbar/>
+<Footer />
     </>
   )
 }
