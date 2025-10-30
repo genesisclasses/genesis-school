7274b57b0bb53ab2e3dd167413de37abe3ae341e
PriyaGupta30302 <priyagupta30302@gmail.com>
2025-10-30 11:57:19 +0530

    create componentfolder  and page folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './global/Navbar'
-import Home from './components/Home'
+import Home from './pages/Home'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<Navbar/>
-<Footer />
+     <Home/>
     </>
   )
 }
