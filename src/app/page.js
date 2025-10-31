17417313b8f52093d7b7e36308abc30005b39f12
riyasham2151 <riyasham2151@gmail.com>
2025-10-31 06:39:37 +0000

    setup for next js done

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <>
+   <h1 className="text-4xl text-red-800"> hello </h1>
+   </>
+  );
+}
--- src/pages/Campus.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Campus = () => {
-  return (
-    <div>Campus</div>
-  )
-}
-
-export default Campus
