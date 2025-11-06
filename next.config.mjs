16cc7dbbc7fabd03d33a185835174d235644bf1a
riyasham2151 <riyasham2151@gmail.com>
2025-11-06 05:55:22 +0000

    next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ["res.cloudinary.com"], // ✅ allow Cloudinary images
     remotePatterns: [
-     {
-        protocol: 'https',
-        hostname: 'rkdyofxkdnakiosdyjis.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/public/**',
+      {
+        protocol: "https",
+        hostname: "rkdyofxkdnakiosdyjis.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/**",
       },
     ],
   },
 };
 
-export default nextConfig;
+export default nextConfig;   // ✅ REQUIRED IN .mjs
